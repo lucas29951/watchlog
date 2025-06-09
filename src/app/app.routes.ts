6eb9bccd167f2a92da9943ab944b9d8f6f15b8fb
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-06-09 20:33:21 -0300

    Especificacion de rutas de redireccion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { DetailComponent } from './features/detail/detail.component';
+import { LibraryComponent } from './features/library/library.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent) },
+    //{ path: '', component: HomeComponent },
+    { path: 'detail/:id', loadComponent: () => import('./features/detail/detail.component').then(m => m.DetailComponent) },
+    { path: 'library', loadComponent: () => import('./features/library/library.component').then(m => m.LibraryComponent) },
+    { path: '**', redirectTo: '' }
+];
