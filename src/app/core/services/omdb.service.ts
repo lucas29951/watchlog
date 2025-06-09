94efd87e48751fcaf9eefe0c126ddab46151ee83
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-06-09 20:28:49 -0300

    Metodos basicos de consulta a la API agregados

--- src/app/core/services/omdb.service.ts
+++ src/app/core/services/omdb.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OmdbService {
 
-  constructor() { }
+  private apiKey = '4a340c34';
+  private url = 'https://www.omdbapi.com/';
+
+  constructor(private http: HttpClient) { }
+
+  searchMedia(query: string): Observable<any> {
+    return this.http.get<any>(`${this.url}?apikey=${this.apiKey}&s=${query}`);
+  }
+
+  getMediaDetails(id: string): Observable<any> {
+    return this.http.get<any>(`${this.url}?apikey=${this.apiKey}&i=${id}&plot=full`);
+  }
 }
