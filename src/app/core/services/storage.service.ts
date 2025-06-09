8057cb11b21a5f04f26742c6b4c70262ac277e68
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-06-09 20:30:00 -0300

    Metodos basicos de manejo de LocalStorage añadidos

--- src/app/core/services/storage.service.ts
+++ src/app/core/services/storage.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Media } from '../models/media.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
 
-  constructor() { }
+  private key = 'watchlog-list';
+
+  getList(): Media[] {
+    return JSON.parse(localStorage.getItem(this.key) || '[]');
+  }
+
+  saveList(list: Media[]): void {
+    localStorage.setItem(this.key, JSON.stringify(list));
+  }
+
+  addToList(item: Media): void {
+    const list = this.getList();
+
+    if (!list.some(m => m.imdbID === item.imdbID)) {
+      list.push(item);
+      this.saveList(list);
+    }
+  }
+
+  removeFromList(id: string): void {
+    const list = this.getList().filter(m => m.imdbID !== id);
+    this.saveList(list);
+  }
 }
