919d2eac77b75868aeab2cd29cbf62a45cd34baa
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-06-10 12:20:15 -0300

    Metodos basicos del componente definidos

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { OmdbService } from '../../core/services/omdb.service';
+import { Media } from '../../core/models/media.model';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MediaCardComponent } from "../../shared/components/media-card/media-card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, MediaCardComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
+  query: string = '';
+  results: Media[] = [];
+  searched = false;
+
+  constructor(private omdb: OmdbService) {}
+
+  search(): void {
+    if (!this.query) return;
+
+    this.omdb.searchMedia(this.query).subscribe(res => {
+      this.searched = true;
+      this.results = res.Search?.map((item: any) => ({
+        imdbID: item.imdbID,
+        Title: item.Title,
+        Year: item.Year,
+        Type: item.Type,
+        Poster: item.Poster,
+        Rating: item.Rating,
+        Genre: item.Genre,
+        Director: item.Director,
+        Plot: item.Plot
+      })) || [];
+    });
+  }
 }
