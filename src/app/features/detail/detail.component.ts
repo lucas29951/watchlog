8258d11aea033d2806b52c4cc60c88e8ab4f3317
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-06-10 12:16:04 -0300

    Logica basica del componente añadida

--- src/app/features/detail/detail.component.ts
+++ src/app/features/detail/detail.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OmdbService } from '../../core/services/omdb.service';
+import { StorageService } from '../../core/services/storage.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './detail.component.html',
-  styleUrl: './detail.component.css'
+  styleUrls: ['./detail.component.css']
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit {
 
+  media: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private omdb: OmdbService,
+    private storage: StorageService
+  ) {}
+
+  ngOnInit(): void {
+      const id = this.route.snapshot.paramMap.get('id');
+
+      if (id) {
+        this.omdb.getMediaDetails(id).subscribe(data => {
+          this.media = data;
+        });
+      }
+  }
+
+  addToList(): void {
+    this.storage.addToList(this.media);
+  }
 }
