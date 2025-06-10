05509ef488a10232eb8df48a7bd3325019dd1640
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-06-10 12:11:05 -0300

    Logica del componente incorporada

--- src/app/shared/components/media-card/media-card.component.ts
+++ src/app/shared/components/media-card/media-card.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { Media } from '../../../core/models/media.model';
 
 @Component({
   selector: 'app-media-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './media-card.component.html',
-  styleUrl: './media-card.component.css'
+  styleUrls: ['./media-card.component.css']
 })
 export class MediaCardComponent {
 
+  @Input() media!: Media;
+
+  constructor(private router: Router) {}
+
+  goToDetail(): void {
+    this.router.navigate(['/detail', this.media.imdbID]);
+  }
 }
