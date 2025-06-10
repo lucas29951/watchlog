5930c3dc33676fae97fe53b6e21c540d2ef6900e
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-06-10 12:13:35 -0300

    Metodos y logica del componente añadidas

--- src/app/features/library/library.component.ts
+++ src/app/features/library/library.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../../core/services/storage.service';
+import { Media } from '../../core/models/media.model';
+import { CommonModule } from '@angular/common';
+import { MediaCardComponent } from "../../shared/components/media-card/media-card.component";
 
 @Component({
   selector: 'app-library',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MediaCardComponent],
   templateUrl: './library.component.html',
-  styleUrl: './library.component.css'
+  styleUrls: ['./library.component.css']
 })
-export class LibraryComponent {
+export class LibraryComponent implements OnInit {
 
+  list: Media[] = [];
+
+  constructor(private storage: StorageService) {}
+
+  ngOnInit(): void {
+      this.list = this.storage.getList();
+  }
+
+  remove(id: string): void {
+    this.storage.removeFromList(id);
+    this.list = this.storage.getList();
+  }
 }
