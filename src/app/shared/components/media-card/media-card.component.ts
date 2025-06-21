db2f305085abd18a67ef406ce535441ed448606b
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-06-21 16:32:47 -0300

    Modificacion de la logica del componente

--- src/app/shared/components/media-card/media-card.component.ts
+++ src/app/shared/components/media-card/media-card.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { Media } from '../../../core/models/media.model';
 import { OmdbService } from '../../../core/services/omdb.service';
+import { StorageService } from '../../../core/services/storage.service';
 
 @Component({
   selector: 'app-media-card',
@@ -14,15 +15,20 @@ import { OmdbService } from '../../../core/services/omdb.service';
 export class MediaCardComponent implements OnInit {
 
   @Input() media!: Media;
+  isInLibrary: boolean = false;
 
   constructor(
     private router: Router,
-    private omdb: OmdbService
+    private omdb: OmdbService,
+    private storage: StorageService
   ) {}
 
   ngOnInit(): void {
       this.omdb.getMediaDetails(this.media.imdbID).subscribe( data => {
         this.media = data;
+
+        const list = this.storage.getList();
+        this.isInLibrary = list.some(m => m.imdbID === data.imdbID);
       });
   }
 
@@ -34,5 +40,18 @@ export class MediaCardComponent implements OnInit {
     if (rating >= 7) return 'green';
     if (rating >= 5) return 'yellow';
     return 'red';
+  }
+
+  toggleLibrary(): void {
+    let list = this.storage.getList();
+
+    if (this.isInLibrary) {
+      list = list.filter(item => item.imdbID !== this.media.imdbID);
+    } else {
+      list.push(this.media);
+    }
+
+    this.storage.saveList(list);
+    this.isInLibrary = !this.isInLibrary;
   }
 }
