cce6a6ab4f4c56be287f9cb4617a444bb50d7bdd
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-06-22 21:09:07 -0300

    Feature: logica de notificacion añadida al componente

--- src/app/features/detail/detail.component.ts
+++ src/app/features/detail/detail.component.ts
@@ -4,11 +4,12 @@ import { OmdbService } from '../../core/services/omdb.service';
 import { StorageService } from '../../core/services/storage.service';
 import { CommonModule } from '@angular/common';
 import { Media } from '../../core/models/media.model';
+import { NotificationComponent } from '../../shared/components/notification/notification.component';
 
 @Component({
   selector: 'app-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NotificationComponent],
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.css']
 })
@@ -16,6 +17,9 @@ export class DetailComponent implements OnInit {
 
   media: any;
   isInLibrary: boolean = false;
+  showNotification: boolean = false;
+  notificationMessage: string = '';
+  notificationType: 'is-success' | 'is-danger' | 'is-info' = 'is-success';
 
   constructor(
     private route: ActivatedRoute,
@@ -52,11 +56,16 @@ export class DetailComponent implements OnInit {
 
     if (this.isInLibrary) {
       list = list.filter(item => item.imdbID !== this.media.imdbID);
+      this.notificationMessage = 'Eliminado de la lista.';
+      this.notificationType = 'is-danger';
     } else {
       list.push(this.media);
+      this.notificationMessage = 'Agregado a la lista.';
+      this.notificationType = 'is-success';
     }
 
     this.storage.saveList(list);
     this.isInLibrary = !this.isInLibrary;
+    this.showNotification = true;
   }
 }
