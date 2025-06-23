155f887a133e01f7af4aff045bbb3c95f0faa447
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-06-22 21:07:40 -0300

    Feature: logica añadida para mostrar la notificacion

--- src/app/features/library/library.component.ts
+++ src/app/features/library/library.component.ts
@@ -3,11 +3,12 @@ import { StorageService } from '../../core/services/storage.service';
 import { Media } from '../../core/models/media.model';
 import { CommonModule } from '@angular/common';
 import { MediaCardComponent } from "../../shared/components/media-card/media-card.component";
+import { NotificationComponent } from '../../shared/components/notification/notification.component';
 
 @Component({
   selector: 'app-library',
   standalone: true,
-  imports: [CommonModule, MediaCardComponent],
+  imports: [CommonModule, MediaCardComponent, NotificationComponent],
   templateUrl: './library.component.html',
   styleUrls: ['./library.component.css']
 })
@@ -15,6 +16,8 @@ export class LibraryComponent implements OnInit {
 
   list: Media[] = [];
   viewMode: 'grid' | 'list' = 'grid';
+  showNotification: boolean = false;
+  notificationMessage: string = '';
 
   constructor(private storage: StorageService) {}
 
@@ -30,6 +33,9 @@ export class LibraryComponent implements OnInit {
   removeFromLibrary(id: string): void {
     this.list = this.list.filter(item => item.imdbID !== id);
     this.storage.saveList(this.list);
+
+    this.notificationMessage = 'Elemento eliminado de la lista.';
+    this.showNotification = true;
   }
 
   switchView(mode: 'grid' | 'list') {
